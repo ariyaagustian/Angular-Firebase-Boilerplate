e0f2af42606e6b1f2c37dc99e3bfc6b087574d98
Ariya Agustian <ariya.riyadh@wgs-id.com>
2020-11-13 11:02:43 +0700

    Update Firebase Environment to Codevibes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: 'AIzaSyDyBV6d-kS7Qd0EgJKjGrm9FRZafwzXdXA',
-    authDomain: 'argon-angular-boilerplate.firebaseapp.com',
-    databaseURL: 'https://argon-angular-boilerplate.firebaseio.com',
-    projectId: 'argon-angular-boilerplate',
-    storageBucket: 'argon-angular-boilerplate.appspot.com',
-    messagingSenderId: '805602749071',
-    appId: '1:805602749071:web:c70aeb3b1d18479303defa'
+    apiKey: 'AIzaSyCFgPWQ_yt092xx3d8mCA47JFCcLpfx8Zc',
+    authDomain: 'codevibes-id.firebaseapp.com',
+    databaseURL: 'https://codevibes-id.firebaseio.com',
+    projectId: 'codevibes-id',
+    storageBucket: 'codevibes-id.appspot.com',
+    messagingSenderId: '9141712637',
+    appId: '1:9141712637:web:34e13242125480cbabf094'
   }
 };
